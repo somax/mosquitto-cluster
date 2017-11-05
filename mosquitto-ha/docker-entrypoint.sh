3f883549fd07443bf7780c2f9d23c7eb226f8fc8
somax <somaxj@163.com>
2017-11-05 23:18:11 +0800

    feat: 简化
    - bridge 与 broker 使用一个容器
    - 根据环境变量容器启动时创建 config

---
+++ mosquitto-ha/docker-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# generate config file
+CONFIG_FILE=/mosquitto/config/mosquitto.conf
+
+cat << EOF > $CONFIG_FILE
+# General Mosquitto Config
+user mosquitto
+allow_duplicate_messages false
+autosave_interval 60
+autosave_on_changes false
+persistence true
+persistence_file mosquitto.db
+persistence_location /mosquitto/data/
+log_dest stderr
+log_type all
+connection_messages true
+log_timestamp true
+listener 1883
+EOF
+
+if [ "$MOSQUITTO_AS" == "ha_broker" ]; 
+then
+
+cat << EOF >> $CONFIG_FILE
+
+# Bridge specific config
+connection $HOSTNAME
+round_robin false
+try_private true
+addresses $MOSQUITTO_BRIDGE_NODES
+start_type automatic
+notifications false
+cleansession false
+topic # out 2 "" $HOSTNAME/
+EOF
+
+IFS=","
+for broker in $MOSQUITTO_OTHER_BROKERS; do
+	echo "topic # in 2" '""' "$broker/" >> $CONFIG_FILE
+done
+
+fi
+
+# from origin docker-entrypoint.sh
+set -e
+exec "$@"
